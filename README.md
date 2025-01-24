4b70362e91ad83e341aad16a4c5816dc30dafa72
bkolosk1 <bosec99@gmail.com>
2025-01-24 08:41:00 +0100

    Move initial package.

---
+++ README.md
@@ -0,0 +1,26 @@
+# HorNets
+
+**HorNets** is a Python package implementing the HorNets architecture of the ``HorNets: Learning from Discrete and Continuous Signals with Routing Neural Networks`` paper.
+
+## Installation
+
+Follow the instructions below to install **HorNets** using your preferred method.
+
+### Prerequisites
+
+Ensure you have the following installed on your system:
+
+- **Python 3.8 or higher**: [Download Python](https://www.python.org/downloads/)
+
+### Using PyPI
+
+You can install **HorNets** directly from PyPI using `pip`. This is the simplest method if you just want to use the package.
+
+```bash
+pip install hornets
+```
+
+## Usage
+
+
+You can run `examples/example.py` 
---
+++ examples/examples.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from hornets import HorNetClassifier, generate_synthetic_data
+
+def main():
+    num_features = 3
+    num_instances = 128
+    operation = "xor"
+    X, y = generate_synthetic_data(num_features=num_features, num_instances=num_instances, operation=operation)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    classifier = HorNetClassifier(
+        num_rules=256,
+        exp_param=4,
+        feature_names=[f"feature_{i}" for i in range(X.shape[1])],
+        activation="polyclip",
+        order=5,
+        learning_rate=0.1,     
+        batch_size=10,
+        stopping_crit=100,
+        num_epochs=300,
+        verbose=True
+    )
+
+    classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred))
+
+
+if __name__ == "__main__":
+    main()
